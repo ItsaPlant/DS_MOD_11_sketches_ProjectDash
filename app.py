74889cbb826890ad9bc056540e4bfd5f8c3e175a
ItsaPlant <krzysiekmichalik6@gmail.com>
2024-02-01 15:07:05 +0100

    update

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ import tab1
 import tab2
 
 
+
 class db:
     def __init__(self):
         self.transactions = db.transation_init()
-        self.cc = pd.read_csv(r'db\country_codes.csv',index_col=0)
-        self.customers = pd.read_csv(r'db\customers.csv',index_col=0)
-        self.prod_info = pd.read_csv(r'db\prod_cat_info.csv')
+        self.cc = pd.read_csv(r'/Users/mac/Desktop/py/MOD_11/MOD_11_PROJEKT/MOD_11_sketches_ProjectDash/db/country_codes.csv',index_col=0)
+        self.customers = pd.read_csv(r'/Users/mac/Desktop/py/MOD_11/MOD_11_PROJEKT/MOD_11_sketches_ProjectDash/db/customers.csv',index_col=0)
+        self.prod_info = pd.read_csv(r'/Users/mac/Desktop/py/MOD_11/MOD_11_PROJEKT/MOD_11_sketches_ProjectDash/db/prod_cat_info.csv')
 
 
     @staticmethod
     def transation_init():
         transactions = pd.DataFrame()
-        src = r'db\transactions'
+        src = r'/Users/mac/Desktop/py/MOD_11/MOD_11_PROJEKT/MOD_11_sketches_ProjectDash/db/transactions'
         for filename in os.listdir(src):
-            transactions = transactions.append(pd.read_csv(os.path.join(src,filename),index_col=0))
+            transactions = pd.concat([transactions, pd.read_csv(os.path.join(src,filename),index_col=0)])
 
         def convert_dates(x):
             try:
@@ -61,12 +62,13 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 #dashboardy
 app.layout = html.Div([html.Div([dcc.Tabs(id='tabs',value='tab-1',children=[
                             dcc.Tab(label='Sprzedaż globalna',value='tab-1'),
-                            dcc.Tab(label='Produkty',value='tab-2')
+                            dcc.Tab(label='Produkty',value='tab-2'),
+                            dcc.Tab(label='Pipelines',value='tab-3')
                             ]),
                             html.Div(id='tabs-content')
                     ],style={'width':'80%','margin':'auto'})],
                     style={'height':'100%'})
-
+print(app)
 
 @app.callback(Output('tabs-content','children'),[Input('tabs','value')])
 def render_content(tab):
@@ -75,6 +77,8 @@ def render_content(tab):
         return tab1.render_tab(df.merged)
     elif tab == 'tab-2':
         return tab2.render_tab(df.merged)
+    elif tab == 'tab-3':
+        return tab3.render_tab(df.merged)
 
 
 ## tab1 callbacks
@@ -119,7 +123,11 @@ def tab1_choropleth_sales(start_date,end_date):
             [Input('prod_dropdown','value')])
 def tab2_barh_prod_subcat(chosen_cat):
 
-    grouped = df.merged[(df.merged['total_amt']>0)&(df.merged['prod_cat']==chosen_cat)].pivot_table(index='prod_subcat',columns='Gender',values='total_amt',aggfunc='sum').assign(_sum=lambda x: x['F']+x['M']).sort_values(by='_sum').round(2)
+    grouped = df.merged[(df.merged['total_amt']>0)&
+    (df.merged['prod_cat']==chosen_cat)].pivot_table(index='prod_subcat',
+                                                    columns='Gender',
+                                                    values='total_amt',
+                                                    aggfunc='sum').assign(_sum=lambda x: x['F']+x['M']).sort_values(by='_sum').round(2)
 
     traces = []
     for col in ['F','M']:
@@ -130,6 +138,48 @@ def tab2_barh_prod_subcat(chosen_cat):
     return fig
 
 
+## tab3 callbacks
+@app.callback(Output('bar-pipe','figure'),
+    [Input('sales-range','start_date'),Input('sales-range','end_date')])
+
+
+def tab1_bar_sales(start_date,end_date):
+
+    truncated = df.merged[(df.merged['tran_date']>=start_date)&(df.merged['tran_date']<=end_date)]
+    grouped = truncated[truncated['total_amt']>0].groupby([pd.Grouper(key='tran_date',freq='M'),'Store_type'])['total_amt'].sum().round(2).unstack()
+
+    traces = []
+    for col in grouped.columns:
+        traces.append(go.Bar(x=grouped.index,y=grouped[col],name=col,hoverinfo='text',
+        hovertext=[f'{y/1e3:.2f}k' for y in grouped[col].values]))
+
+    data = traces
+    fig = go.Figure(data=data,layout=go.Layout(title='Przychody',barmode='stack',legend=dict(x=0,y=-0.5)))
+
+    return fig
+
+
+@app.callback(Output('choropleth-pipe','figure'),
+            [Input('sales-range','start_date'),Input('sales-range','end_date')])
+def tab1_choropleth_sales(start_date,end_date):
+
+    truncated = df.merged[(df.merged['tran_date']>=start_date)&(df.merged['tran_date']<=end_date)]
+    grouped = truncated[truncated['total_amt']>0].groupby('country')['total_amt'].sum().round(2)
+
+    trace0 = go.Choropleth(colorscale='Viridis',reversescale=True,
+                            locations=grouped.index,locationmode='country names',
+                            z = grouped.values, colorbar=dict(title='Sales'))
+    data = [trace0]
+    fig = go.Figure(data=data,layout=go.Layout(title='Mapa',geo=dict(showframe=False,projection={'type':'natural earth'})))
+
+    return fig
+
+USERNAME_PASSWORD = [['user','pass']]
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD)
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8080)
